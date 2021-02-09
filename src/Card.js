5dc2dbe074b077d44db8214ed7226e1154edf546
Lauri <phantom.loop.games@gmail.com>
2021-02-09 11:46:48 +0200

    Fixing

--- src/Card.js
+++ src/Card.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const Card = (props) => {
+const Card = ({ name, email, id }) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt={`weird friend ${props.id}`} src={`https://robohash.org/set_set5/${props.id}?200x200`} />
+            <img alt={`weird friend ${id}`} src={`https://robohash.org/set_set5/${id}?200x200`} />
         <div>
-            <h2>{props.name}</h2>
-            <p>{props.email}</p>
+            <h2>{name}</h2>
+            <p>{email}</p>
             </div>
         </div>
     );
